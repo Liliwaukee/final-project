dcc067663862e9cdd9869c7108e2612dc68c45a6
liliwaukee <lilimmvisual@gmail.com>
2017-10-31 17:52:47 +0000

    Cigrado Cesar ya no acepta números

--- caesar-cipher/js/app.js
+++ caesar-cipher/js/app.js
@@ -1,64 +1,71 @@
 //creamos la función para cifrar
 function cipher (){
-//pedimos la frase al usuario y lo convertimos en array
-var userPhrase = prompt("Ingresa la frase que quieras cifrar");
-var arrayPhrase = userPhrase.split("");
+  //pedimos la frase al usuario y lo convertimos en array
+  var userPhrase = prompt("Ingresa la frase que quieras cifrar");
+  var arrayPhrase = userPhrase.split("");
 
-var toAscii = [];
-var arrayCipher = [];
-var toAsciiRest = [];
+  for(var z = 0; z <arrayPhrase.length; z++){
+     if(arrayPhrase[z] == 1 || arrayPhrase[z] == 2 || arrayPhrase[z] == 3 || arrayPhrase[z] == 4 || arrayPhrase[z] == 5 || arrayPhrase[z] == 6 ||arrayPhrase[z] == 7 ||arrayPhrase[z] == 8 ||arrayPhrase[z] == 9 || arrayPhrase[z] === 0 || arrayPhrase[z] == " " ){
+     return alert("No puedes ingresar números, ni espacios");
+      }
+  }
 
-// creamos un for para entrar al array y sacar el código ascii de cada letra y lo guardamos en un nuevo array
-for (var i = 0; i< arrayPhrase.length; i++){
-  var letras = arrayPhrase[i].charCodeAt();
-  //dependiendo del código ascii de la letra le vamos a sumar 7 o le vamos arestar 19
-    if(letras < 116){
-      toAscii.push(letras + 7);
-    } else {
-      toAscii.push(letras - 19);
-    }
-}
-//creamos otro for que entre al array de código ascii y le pedimos que lo convierta en carácter y lo guardamos en un nueva variable
-for (var j = 0; j < toAscii.length; j++){
-  var result = String.fromCharCode(toAscii[j]);
-      arrayCipher.push(result);
-}
-//pasamos el array a strin para mostrarlo al usuario
-var textCipher = arrayCipher.join("");
-return document.write(textCipher);
+  var toAscii = [];
+  var arrayCipher = [];
+  var toAsciiRest = [];
+
+  // creamos un for para entrar al array y sacar el código ascii de cada letra y lo guardamos en un nuevo array
+  for (var i = 0; i< arrayPhrase.length; i++){
+    var letras = arrayPhrase[i].charCodeAt();
+    //dependiendo del código ascii de la letra le vamos a sumar 7 o le vamos arestar 19
+      if(letras < 116){
+        toAscii.push(letras + 7);
+      } else {
+        toAscii.push(letras - 19);
+      }
+  }
+  //creamos otro for que entre al array de código ascii y le pedimos que lo convierta en carácter y lo guardamos en un nueva variable
+  for (var j = 0; j < toAscii.length; j++){
+    var result = String.fromCharCode(toAscii[j]);
+        arrayCipher.push(result);
+  }
+
+  //pasamos el array a strin para mostrarlo al usuario
+  var textCipher = arrayCipher.join("");
+  return document.write("El texto cifrado es: " + textCipher);
 
 }
 
 //aquí empieza la función decipher
-
 function decipher (){
-//pedirmos la frase cifrada y la convertimos en array
-var userPhrase = prompt("Ingresa la frase que quieras decifrar");
-var arrayPhrase = userPhrase.split("");
+  //pedirmos la frase cifrada y la convertimos en array
+  var userPhrase = prompt("Ingresa la frase que quieras decifrar");
+  var arrayPhrase = userPhrase.split("");
 
-var toAscii = [];
-var arrayCipher = [];
-var toAsciiRest = [];
+  var toAscii = [];
+  var arrayCipher = [];
+  var toAsciiRest = [];
 
-//entramos al array de caracteres y pedimos su código ascii, dependiendo del número le sumaremos 19 o le restaremos 7, lo inverso a la función cifrar
-for (var i = 0; i< arrayPhrase.length; i++){
-  var letras = arrayPhrase[i].charCodeAt();
-    if(letras < 104){
-      toAscii.push(letras + 19);
-    } else {
-      toAscii.push(letras - 7);
-    }
+  //entramos al array de caracteres y pedimos su código ascii, dependiendo del número le sumaremos 19 o le restaremos 7, lo inverso a la función cifrar
+  for (var i = 0; i< arrayPhrase.length; i++){
+    var letras = arrayPhrase[i].charCodeAt();
+      if(letras < 104){
+        toAscii.push(letras + 19);
+      } else {
+        toAscii.push(letras - 7);
+      }
+  }
+  //entramos al array de código ascii y le pedimos que lo convierta en carácter
+  for (var j = 0; j < toAscii.length; j++){
+    var result = String.fromCharCode(toAscii[j]);
+        arrayCipher.push(result);
+  }
+
+  //pasamos el array a string y lo mostramos al usuario
+  var textCipher = arrayCipher.join("");
+  return document.write("El texto descifrado es: " + textCipher);
+
 }
-//entramos al array de código ascii y le pedimos que lo convierta en carácter
-for (var j = 0; j < toAscii.length; j++){
-  var result = String.fromCharCode(toAscii[j]);
-      arrayCipher.push(result);
-}
-//pasamos el array a string y lo mostramos al usuario
-var textCipher = arrayCipher.join("");
-return document.write(textCipher);
 
-}
-
-//cipher();
-decipher ();
+cipher();
+//decipher ();
--- valid-credit-card/js/app.js
+++ valid-credit-card/js/app.js
@@ -2,52 +2,53 @@
 function isValid() {
   var number = prompt("ingresa el numero");//pedimos elnúmero de tarjeta delusuario
 // si hay espacios, no debe seguir la función
-for(var z = 0; z<number.length; z++){
-  if(number[z] === " " || number[z] === "" ){
-    return document.write("No puedes ingresar espacios en blanco");
-  } else {
-
-  var cardNumber = number.split(""); //convertimos en array
-  var inverse = cardNumber.reverse();//87541... números al revés
-//console.log(inverse);
-//declaramosvariables vacías para después almacenar los resultados
-  var odd = 0;
-  var even = 0;
-  var evenPlus = 0;
-
-
-for(var i = 0; i <16; i++){ //aquí sacamos la suma de numeros impares
-  if(i % 2 !== 1){
-     odd = odd + parseInt(inverse[i]);
-  }
-}
-
-for(var j = 1; j <16; j++){
-  //sacamos el números pares menores a 4 y multiplicamos por 2
-  if (j % 2 === 1 && parseInt(inverse[j]) <= 4){
-    even = even + (parseInt(inverse[j])*2);
-    // de aquí en adelante reasiganamos los valores mayores del
-  } else if (j % 2 === 1 && inverse[j] == 5){
-     evenPlus = evenPlus + parseInt(1);
-  } else if (j % 2 === 1 && inverse[j] == 6){
-     evenPlus = evenPlus + parseInt(3);
-  } else if (j % 2 === 1 && inverse[j] == 7){
-     evenPlus = evenPlus + parseInt(5);
-  } else if (j % 2 === 1 && inverse[j] == 8){
-     evenPlus = evenPlus + parseInt(7);
-  } else if (j % 2 === 1 && inverse[j] == 9){
-     evenPlus = evenPlus + parseInt(9);
-  }
-}
-//hacemos la de nuestros numeros pares, pares mayores e impares y comprobamos si son módulo de 10
-var ckeckCard = even + odd + evenPlus;
-    if(ckeckCard % 10 === 0){
-      return document.write("It´s a valid card");
+  for(var z = 0; z<number.length; z++){
+    if(number[z] === " " || number[z] === "" ){
+      return document.write("No puedes ingresar espacios en blanco");
     } else {
-      return document.write("Your card is an invalid card");
-    }
+
+        var cardNumber = number.split(""); //convertimos en array
+        var inverse = cardNumber.reverse();//87541... números al revés
+      //console.log(inverse);
+      //declaramosvariables vacías para después almacenar los resultados
+        var odd = 0;
+        var even = 0;
+        var evenPlus = 0;
+
+
+        for(var i = 0; i <16; i++){ //aquí sacamos la suma de numeros impares
+          if(i % 2 !== 1){
+             odd = odd + parseInt(inverse[i]);
+          }
+        }
+
+        for(var j = 1; j <16; j++){
+          //sacamos el números pares menores a 4 y multiplicamos por 2
+          if (j % 2 === 1 && parseInt(inverse[j]) <= 4){
+            even = even + (parseInt(inverse[j])*2);
+            // de aquí en adelante reasiganamos los valores mayores del
+          } else if (j % 2 === 1 && inverse[j] == 5){
+             evenPlus = evenPlus + parseInt(1);
+          } else if (j % 2 === 1 && inverse[j] == 6){
+             evenPlus = evenPlus + parseInt(3);
+          } else if (j % 2 === 1 && inverse[j] == 7){
+             evenPlus = evenPlus + parseInt(5);
+          } else if (j % 2 === 1 && inverse[j] == 8){
+             evenPlus = evenPlus + parseInt(7);
+          } else if (j % 2 === 1 && inverse[j] == 9){
+             evenPlus = evenPlus + parseInt(9);
+          }
+        }
+
+      //hacemos la de nuestros numeros pares, pares mayores e impares y comprobamos si son módulo de 10
+      var ckeckCard = even + odd + evenPlus;
+          if(ckeckCard % 10 === 0){
+            return document.write("It´s a valid card");
+          } else {
+            return document.write("Your card is an invalid card");
+          }
+      }
   }
-}
 }
 
 isValid();
